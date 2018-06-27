3cca79f8ac3c7c58c9c493ddd11a8440066f3944
jorgereina1986 <jorgereina1986@gmail.com>
2018-06-26 23:10:58 -0400

    modified adapter

--- app/src/main/java/com/jorgereina/calendars/dayfragment/DayAdapter.java
+++ app/src/main/java/com/jorgereina/calendars/dayfragment/DayAdapter.java
@@ -3,10 +3,8 @@ package com.jorgereina.calendars.dayfragment;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
-import com.jorgereina.calendars.calendarfragment.MonthAdapter;
 import com.jorgereina.calendars.databinding.EventItemBinding;
 import com.jorgereina.calendars.model.Event;
 
